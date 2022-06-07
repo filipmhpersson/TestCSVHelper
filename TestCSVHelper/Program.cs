b716a89252a60d097b77c28dd286595f96f7ef14
Filip Persson <filip.persson@ibinder.com>
2022-06-07 13:40:16 +0200

    Add project files.

---
+++ TestCSVHelper/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TestCSVHelper;
+
+Console.WriteLine("Hello, World!");
+
+var tenderBid = new TenderBid
+{
+    BidSubmitted = true,
+    Id = "1",
+    TenderId = "2"
+};
+
+var tenderBid2 = new TenderBid
+{
+    Id = "2",
+    TenderId = "2",
+    BidSubmitted = false
+};
+
+using var reader = new StreamWriter(".\\test.csv");
+
+using var csv = new CsvHelper.CsvWriter(reader, System.Globalization.CultureInfo.InvariantCulture);
+csv.Context.Configuration.HasHeaderRecord= true;
+csv.Context.RegisterClassMap<TenderBidCsvMap>();
+
+csv.WriteRecords(new List<TenderBid>() { tenderBid, tenderBid2 });
---
+++ TestCSVHelper/TenderBid.cs
@@ -0,0 +1,37 @@
+﻿using CsvHelper.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCSVHelper
+{
+
+    public class TenderBidCsvMap : ClassMap<TenderBid>
+    {
+        public TenderBidCsvMap()
+        {
+
+            AutoMap(CultureInfo.InvariantCulture)¨;
+
+        }
+    }
+    public class TenderBid
+    {
+        public string Id { get; set; }
+        public string TenderId { get; set; }
+        public bool BidSubmitted { get; set; }
+        public List<TenderBidPrice> Price { get; set; }
+
+    }
+
+    public class TenderBidPrice
+    {
+        public Guid TenderPriceId { get; set; }
+        public string Price { get; set; }
+        public string Reservation { get; set; }
+    }
+}
